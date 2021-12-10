4c3b5b0a78267cbf1550fc83918b1e78e04af6c1
jieunee2 <980lje@naver.com>
2021-12-10 19:24:13 +0900

    [#5] Entity, Repository 생성

---
+++ src/main/java/com/wherewego/entity/Continent.java
@@ -0,0 +1,36 @@
+package com.wherewego.entity;
+
+import com.wherewego.enumtype.ContinentName;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "continent")
+public class Continent {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "continent_id")
+  private Long id;
+
+  @Column
+  @Enumerated(EnumType.STRING)
+  private ContinentName continentName; //대륙명
+
+
+
+}
