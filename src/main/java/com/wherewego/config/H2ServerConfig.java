96b0950771efe07a744528e6a37b2e09c3188351
jieunee2 <980lje@naver.com>
2021-12-13 17:08:19 +0900

    [#8] 외부에서 H2 접속 가능하게 코드 수정

---
+++ src/main/java/com/wherewego/config/H2ServerConfig.java
@@ -0,0 +1,16 @@
+package com.wherewego.config;
+
+import java.sql.SQLException;
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2ServerConfig {
+
+  @Bean(initMethod = "start", destroyMethod = "stop")
+  public Server H2DatabaseServer() throws SQLException {
+    // 클라이언트에서 jdbc:h2:tcp://localhost:9091/mem:wherewego 경로로 접속 가능
+    return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9091");
+  }
+}
